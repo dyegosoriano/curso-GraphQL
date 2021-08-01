e7c5737f520856aea4a9a911f2626c8c1a57adf9
Dyego Soriano <dyegodf2@gmail.com>
2021-08-01 20:07:33 -0300

    feat: iniciando apollo-server

--- src/server.js
+++ src/server.js
@@ -1,1 +1,10 @@
-console.log({ ok: true })
+import { ApolloServer } from 'apollo-server'
+
+import { resolvers, typeDefs } from './graphql/schema'
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers
+})
+
+server.listen(4000).then(({ url }) => console.log(`Server listening on url ${url}`))
